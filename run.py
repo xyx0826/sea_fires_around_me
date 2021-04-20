2f70026fc91de77fc901b64871e26314a9d2f795
xyx0826 <xyx0826@hotmail.com>
2021-04-19 17:16:23 -0700

    Implement entry point

--- run.py
+++ run.py
@@ -1,5 +1,31 @@
+# run.py: reads configuration and starts the watcher.
+
+import json
+import os
+import time
+
+from realtime import RealTime
+
+
 def main():
-    pass
+    # Check config file
+    if not os.path.isfile("config.json"):
+        print(
+            "Error: could not find config.json."
+            "Make sure you have written your API key and origin into "
+            "config.json.sample and renamed it to config.json."
+        )
+        return
+
+    cfg = None
+    with open("config.json", "r") as f:
+        cfg = json.load(f)
+
+    rt = RealTime(
+        cfg["origin_lat"], cfg["origin_lon"], cfg["mapquest_api_key"])
+    while True:
+        rt.update()
+        time.sleep(20)
 
 
 if __name__ == "__main__":
